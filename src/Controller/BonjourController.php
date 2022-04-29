70036ab1ef95c6f2142abb070c292eda6f986b89
Bastien <bastien.b200405@gmail.com>
2022-04-29 23:48:59 +0200

    Add Bonjour Controller

---
+++ src/Controller/BonjourController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BonjourController extends AbstractController
+{
+    #[Route('/bonjour', name: 'app_bonjour')]
+    public function index(): Response
+    {
+        return $this->render('bonjour/index.html.twig', [
+            'controller_name' => 'BonjourController'
+        ]);
+    }
+}
