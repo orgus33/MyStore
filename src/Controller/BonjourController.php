a303880c6c7f3b4053024b6b9baa1111ba0979a5
Bastien <bastien.b200405@gmail.com>
2022-04-30 00:09:34 +0200

    Amélioration de la page

--- src/Controller/BonjourController.php
+++ src/Controller/BonjourController.php
@@ -12,7 +12,7 @@ class BonjourController extends AbstractController
     public function index(): Response
     {
         return $this->render('bonjour/index.html.twig', [
-            'controller_name' => 'BonjourController'
+            "name" => "Bastien"
         ]);
     }
 }
